cb43aca2eb164e0871ff14c4fa70b0c60b3316e9
nmh2003 <nguyen.minh.hieu.sinhnam2k3@gmail.com>
2023-02-23 19:08:16 +0700

    commit note

---
+++ BaiThucHanh2102/Slide35.java
@@ -0,0 +1,18 @@
+package Demo.BaiThucHanh2102;
+
+public class Slide35 {
+    import java.util.Scanner;
+
+public class Statement {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if (a >= b) {
+			System.out.println("a lớn hơn hoặc bằng b");
+		} else {
+			System.out.println("a bé hơn b");
+		}
+	}
+}
+}
