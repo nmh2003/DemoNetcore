faef3560d274500b34bdf503a9f43e7ba5427777
nmh2003 <nguyen.minh.hieu.sinhnam2k3@gmail.com>
2023-02-23 19:05:35 +0700

    commit note

---
+++ BaiThucHanh2102/Slide30.java
@@ -0,0 +1,19 @@
+package Demo.BaiThucHanh2102;
+
+public class Slide30 {
+    public static void main(String[] args) {
+        int n, soDu, tong = 0;
+        Scanner scanner = new Scanner(System.in);
+             
+        System.out.println("Nhập vào số nguyên dương bất kỳ: ");
+        n = scanner.nextInt();
+             
+        while (n > 0) {
+            soDu = n % 10;
+            n = n / 10;
+            tong += soDu;
+        }
+             
+        System.out.println("Tổng các chữ số = " + tong);
+    }
+}
