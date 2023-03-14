0229da7d1cc066a26a0be12eab6f5807c4ef35a2
nmh2003 <nguyen.minh.hieu.sinhnam2k3@gmail.com>
2023-03-14 14:56:04 +0700

    commit note

--- BaiThucHanhLab2/Bai2.java
+++ BaiThucHanhLab2/Bai2.java
@@ -1,5 +1,17 @@
 package Demo.BaiThucHanhLab2;
 
 public class Bai2 {
-    
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhập vào số cần kiểm tra n:");
+        n = scanner.nextInt();
+        // thực hiện câu lệnh if else để kiểm tra điều kiện
+        if (n % 2 == 0) {
+            System.out.println(n + " là số chẵn!");
+        } else {
+            System.out.println(n + " là số lẻ");
+        }
+    }
+}
 }
