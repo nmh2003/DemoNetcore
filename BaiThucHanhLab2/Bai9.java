735683b4c1a94b222aae54c7d83a9cfa45baba97
nmh2003 <nguyen.minh.hieu.sinhnam2k3@gmail.com>
2023-03-14 15:23:00 +0700

    commit note

--- BaiThucHanhLab2/Bai9.java
+++ BaiThucHanhLab2/Bai9.java
@@ -1,5 +1,35 @@
 package Demo.BaiThucHanhLab2;
 
 public class Bai9 {
-    
+    public static void main(String[] args) {
+        String chuoi;
+        int soKyTuInHoa = 0, soKyTuInThuong = 0, soChuSo = 0;
+        Scanner scanner = new Scanner(System.in);
+            
+        do {
+            System.out.println("Nhập vào 1 chuỗi bất kỳ: ");
+            chuoi = scanner.nextLine();
+        } while (chuoi.length() > 80);
+            
+       
+        for (int i = 0; i < chuoi.length(); i++) {
+            
+            if (Character.isUpperCase(chuoi.charAt(i))) {
+                soKyTuInHoa++;
+            }
+                
+            if (Character.isLowerCase(chuoi.charAt(i))) {
+                soKyTuInThuong++;
+            }
+                
+            if (Character.isDigit(chuoi.charAt(i))) {
+                soChuSo++;
+            }
+        }
+            
+        System.out.println("Trong chuỗi " + chuoi +
+            " có " + soKyTuHoa + " ký tự hoa," +
+            " có " + soKyTuThuong + " ký tự thường" +
+            " và có " + soChuSo + " số.");
+    }
 }
