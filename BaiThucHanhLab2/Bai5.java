b306373a0e6ff4d75c35c2158795603ee9bb1daf
nmh2003 <nguyen.minh.hieu.sinhnam2k3@gmail.com>
2023-03-14 15:13:47 +0700

    commit note

--- BaiThucHanhLab2/Bai5.java
+++ BaiThucHanhLab2/Bai5.java
@@ -1,5 +1,18 @@
 package Demo.BaiThucHanhLab2;
 
 public class Bai5 {
-    
+    public static void main(String[] args) {
+        float sum = 0, number;
+        Scanner scanner = new Scanner(System.in);
+             
+        do {
+            System.out.println("Nhập số:");
+            number = scanner.nextFloat();
+            sum += number;
+            if (sum > 100)
+                break;
+        } while (number > 0);
+             
+        System.out.println("Tổng = " + sum);
+    }
 }
