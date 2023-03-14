80810aaad938bf231ea0725dd98e54ae9e438ee8
nmh2003 <nguyen.minh.hieu.sinhnam2k3@gmail.com>
2023-03-14 15:00:57 +0700

    commit note

--- BaiThucHanhLab2/Bai3.java
+++ BaiThucHanhLab2/Bai3.java
@@ -1,5 +1,29 @@
 package Demo.BaiThucHanhLab2;
 
 public class Bai3 {
-    
+    public static void main(String[] args) {
+        int age, year;
+        String name, ageGroup;
+        Scanner scanner = new Scanner(System.in);
+         
+        System.out.println("Nhập tên: ");
+        name = scanner.nextLine();
+        System.out.println("Nhập năm sinh: ");
+        year = scanner.nextInt();
+         
+        // Xác định độ tuổi tương ứng
+        age = 2023 - year;  // 2023 là năm hiện tại
+        if (age < 16) {
+            ageGroup = "vị thành niên.";
+        } else if (age >= 16 || age < 18) {
+            ageGroup = "tuổi trưởng thành.";
+        } else {
+            ageGroup = "đã già";
+        }
+         
+        System.out.println("Bạn " + name + " ở độ tuổi " + ageGroup);
+    }
+
+ 
+}
 }
