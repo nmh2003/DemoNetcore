e78d770732280db72e67e2398a3f8b4187cbf354
nmh2003 <nguyen.minh.hieu.sinhnam2k3@gmail.com>
2023-03-14 15:15:49 +0700

    commit note

--- BaiThucHanhLab2/Bai6.java
+++ BaiThucHanhLab2/Bai6.java
@@ -1,5 +1,24 @@
 package Demo.BaiThucHanhLab2;
 
 public class Bai6 {
-    
+    public static void main(String[] args) {
+        int number, temp = 1;
+        long giaiThua = 1;
+         
+        Scanner scanner = new Scanner(System.in);
+         
+        do {
+            System.out.println("Nhập số: ");
+            number = scanner.nextInt();
+        } while ((number <= 0) || (number > 10));
+         
+        while (temp <= number) {
+            giaiThua *= temp;
+            temp++;
+        }
+         
+        System.out.println(number + "! = " + giaiThua);
+    }
+ 
+}
 }
