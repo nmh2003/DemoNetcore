381421eee7fa61e092e8f4923ac293edeacedd31
nmh2003 <nguyen.minh.hieu.sinhnam2k3@gmail.com>
2023-04-09 13:52:16 +0700

    commit note

---
+++ BaiThucHanhLab3/App.java
@@ -0,0 +1,9 @@
+package Demo.BaiThucHanhLab3;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        HinhTron hinhTron = new HinhTron();
+        hinhTron.NhapBanKinh();
+        hinhTron.Tinh();
+        hinhTron.In();
+}
